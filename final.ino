5263d378fd50c38c02b79daa02bfc72cf86b9814
Ankur Anant <ankuranant99@gmail.com>
2019-04-04 01:38:30 +0530

    minor changes

--- final.ino
+++ final.ino
@@ -1,7 +1,7 @@
 #include <Servo.h> 
 #include <NewPing.h>
 
-int n; 
+int n;
 int duration, distance;
 int trigPin = (10);
 int echoPin = (12);
@@ -18,9 +18,11 @@ void setup() {
   myservo2.attach(9);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(11, OUTPUT); //head led
+  pinMode(13, OUTPUT); //tail led
   pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT); //servo 1 signal
+  pinMode(6, OUTPUT); //servo 2 signal
 }
 
 void loop() {
@@ -31,27 +33,39 @@ void loop() {
   distance = (duration/2) / 29.1;
   Serial.println(distance);
         
-  if (distance < 30) {
+  if (distance < 40) {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100);
+    digitalWrite(13, HIGH);
+    digitalWrite(11, LOW);
+    delay(100);
     digitalWrite(3, HIGH);
     myservo1.write(n);
     myservo2.write(180-n);
     Serial.println("stop");
     delay(500);
-    //myservo1.write(n);
-    //myservo2.write(90-n);
+    //go back
     digitalWrite(5, LOW);
     myservo1.write(n);
     digitalWrite(6, HIGH);
     myservo2.write(90);
-    delay(250);
+    delay(1400);
+    //turn around
+    digitalWrite(5, HIGH);
+    myservo1.write(0);
+    digitalWrite(6, HIGH);
+    myservo2.write(90);
+    delay(1500);
   }
   else {
+    digitalWrite(13, LOW);
+    digitalWrite(11, HIGH);
     digitalWrite(3, LOW);
     Serial.println("regular");
-    myservo1.write(180-n);
-    //myservo2.write(n);
     digitalWrite(5, HIGH);
-    myservo1.write(180);
+    myservo1.write(n);
     digitalWrite(6, LOW);
     myservo2.write(180);
   }
